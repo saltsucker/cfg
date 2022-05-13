f4ca60d6e135c2add4fd20c9d3e24dfc9eb4c02d
Glenn Oberlander <goberlan@protonmail.com>
2022-05-12 22:43:31 -0700

    added more aliases and changed config to cf

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,19 +1,19 @@
-git clone --bare git@github.com:saltsucker/.cfgt.git $HOME/.cfg
-function config {
+git clone --bare git@github.com:saltsucker/cfg.git ~/.cfg
+function cf {
 	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
 mkdir -p .config-backup
 # this puts all the files into the working directory
-config checkout
+cf checkout
 
 if [ $? = 0 ]; then
 	echo "Checked out config.";
 else
 	echo "Backing up pre-existing dot files.";
-	config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+	cf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 
 # I don't htink this one is needed... If it succeeds then you don't need this again. it should be in the else statement I would think?
-config checkout
-config config status.showUntrackedFiles no
+cf checkout
+cf config status.showUntrackedFiles no
